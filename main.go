1c7158e99821a3d84c7b42dccf8158239ef10daa
dortlii <22558338+dortlii@users.noreply.github.com>
2024-09-21 23:39:43 +0200

    feat: Add the helm generation func

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/dortlii/helm-chart-generator/generator"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	helmChart, err := generator.NewHelmChart("2", "myChart", "1.0.0", "application")
+	if err != nil {
+		log.Fatalf("Failed to create helm chart: %s", err)
+	}
+
+	fmt.Printf("This is my helm chart: %s", helmChart)
 }
