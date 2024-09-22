13b6a9ed0ebf23a9d20e1a94508a29c762add87a
dortlii <22558338+dortlii@users.noreply.github.com>
2024-09-22 14:44:20 +0200

    feat: Add comments and helper functions to create and fill a helm chart

--- generator/generator.go
+++ generator/generator.go
@@ -2,26 +2,49 @@ package generator
 
 import "github.com/dortlii/helm-chart-generator/helm"
 
+// NewHelmChart takes parameters to create a helm.Helm object and returns it
 func NewHelmChart(apiVersion, name, version, helmType string) (helm.Helm, error) {
 	chart := fillChart(apiVersion, name, version, helmType)
 	values := helm.NewValues()
 	template := helm.NewTemplate()
 
-	helm := helm.Helm{
+	helmChart := helm.Helm{
 		Chart:    chart,
 		Values:   values,
 		Template: template,
 	}
 
-	return helm, nil
+	return helmChart, nil
 }
 
+// fillChart creates a new, empty helm.Chart object, fills it with default
+// values and overrides them with given values
 func fillChart(apiVersion, name, version, helmType string) helm.Chart {
 	chart := helm.NewChart()
-	chart.ApiVersion = apiVersion
-	chart.Name = name
-	chart.Version = version
-	chart.Type = helmType
+
+	chart = *helm.SetDefaults(chart)
+
+	if checkEmptyField(apiVersion) {
+		chart.ApiVersion = apiVersion
+	}
+
+	if checkEmptyField(name) {
+		chart.Name = name
+	}
+
+	if checkEmptyField(version) {
+		chart.Version = version
+	}
+
+	if checkEmptyField(helmType) {
+		chart.Type = helmType
+	}
 
 	return chart
+}
+
+// checkEmptyField checks the given field, if it's empty
+// and returns a bool
+func checkEmptyField(field string) bool {
+	return field != ""
 }
