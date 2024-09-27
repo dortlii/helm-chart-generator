fd82c04727eae6eaef4efca867baf44a6459b388
dortlii <22558338+dortlii@users.noreply.github.com>
2024-09-28 01:04:39 +0200

    chore: Add first version of Save implementation for Helm

--- helm/helm.go
+++ helm/helm.go
@@ -2,12 +2,22 @@ package helm
 
 // Helm chart components
 type Helm struct {
-	Chart    Chart    `yaml:"chart"`
+	// Chart of the helm chart
+	Chart Chart `yaml:"chart"`
+	// Values of the helm chart
+	Values Values `yaml:"values"`
+	// Template of the helm chart
 	Template Template `yaml:"template"`
-	Values   Values   `yaml:"values"`
 }
 
 // ComponentService exposes file operations
 type ComponentService interface {
 	Save(path string)
+}
+
+// Save implements the function how to save Values to the disk
+func (h Helm) Save(path string) {
+	h.Chart.Save(path)
+	h.Values.Save(path)
+	h.Template.Save(path)
 }
